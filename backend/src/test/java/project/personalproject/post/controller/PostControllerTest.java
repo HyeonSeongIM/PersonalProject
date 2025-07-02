96b9b1f2fec16f1f563ed5054125b17f9af774a4
HyeonSeong <gus8131@naver.com>
2025-07-02 15:33:27 +0900

    [#6] Feat : Post 게시글 생성 controller 테스트

---
+++ backend/src/test/java/project/personalproject/post/controller/PostControllerTest.java
@@ -0,0 +1,60 @@
+package project.personalproject.post.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import project.personalproject.post.dto.request.PostRequest;
+import project.personalproject.post.service.PostService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(PostController.class)
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private PostService postService;
+
+    @Test
+    void 게시글_생성_API_200응답_여부_확인() throws Exception {
+        // given
+        PostRequest postRequest = new PostRequest("title", "content");
+
+        // when & then
+        mockMvc.perform(post("/api/v1/post/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(postRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void 게시글_생성_시_값을_잘_받아오는지_확인() throws Exception {
+        // given
+        PostRequest postRequest = new PostRequest("title", "content");
+
+        given(postService.create(any(PostRequest.class))).willReturn(1L);
+
+        // when & then
+        mockMvc.perform(post("/api/v1/post/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(postRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1)); // JsonBody 에서 ID 필드가 있을 것이고, 거기서 1일거야 를 가르킴
+
+        verify(postService).create(any(PostRequest.class));
+    }
+
+}
