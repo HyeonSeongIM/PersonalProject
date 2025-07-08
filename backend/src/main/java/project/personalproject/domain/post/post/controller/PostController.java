36e7f3e98736a322e24f12b551fb5f1202d8e7ae
HyeonSeong <gus8131@naver.com>
2025-07-08 13:02:56 +0900

    [#7] Feat : 게시글 업데이트, 삭제 컨트롤러 구성

--- backend/src/main/java/project/personalproject/domain/post/post/controller/PostController.java
+++ backend/src/main/java/project/personalproject/domain/post/post/controller/PostController.java
@@ -3,12 +3,10 @@ package project.personalproject.domain.post.post.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.personalproject.domain.member.entity.Member;
 import project.personalproject.domain.post.post.dto.request.CreatePostCommand;
+import project.personalproject.domain.post.post.dto.request.UpdatePostCommand;
 import project.personalproject.domain.post.post.dto.response.PostResponse;
 import project.personalproject.domain.post.post.service.PostService;
 import project.personalproject.global.security.jwt.JwtService;
@@ -24,7 +22,31 @@ public class PostController {
     @PostMapping("/create")
     public ResponseEntity<PostResponse> postCreate(@RequestBody CreatePostCommand postRequest,
                                                    HttpServletRequest request) {
-        Member member = jwtService.getMemberFromToken(request);
-        return ResponseEntity.ok(postService.createPost(postRequest, member));
+        return ResponseEntity.ok(postService.createPost(postRequest, getMemberFromToken(request)));
+    }
+
+    @PatchMapping("/update/{postId}")
+    public ResponseEntity<PostResponse> postUpdate(@RequestBody UpdatePostCommand postRequest,
+                                                   @PathVariable("postId") Long postId,
+                                                   HttpServletRequest request) {
+        return ResponseEntity.ok(postService.updatePost(
+                postId,
+                postRequest,
+                getMemberFromToken(request))
+        );
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<PostResponse> postDelete(@PathVariable("postId") Long postId,
+                                                   HttpServletRequest request) {
+        return ResponseEntity.ok(postService.deletePost(postId, getMemberFromToken(request)));
+    }
+
+
+
+
+
+    private Member getMemberFromToken(HttpServletRequest request) {
+        return jwtService.getMemberFromToken(request);
     }
 }
