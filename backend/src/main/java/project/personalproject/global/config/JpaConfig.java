6b53cca939fb719d1a998079f63dcfd531bca713
HyeonSeong <gus8131@naver.com>
2025-07-28 18:41:35 +0900

    [#27] Feat :  GraphQL 예제 적용 완료

---
+++ backend/src/main/java/project/personalproject/global/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package project.personalproject.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "project.personalproject.domain")
+public class JpaConfig {
+}
