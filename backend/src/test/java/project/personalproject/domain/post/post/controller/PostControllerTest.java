6054a6f10a8d039002f309a113d9c2699f64c5e5
HyeonSeong <gus8131@naver.com>
2025-08-14 16:40:47 +0900

    [#31] Feat : 게시글 생성 테스트 코드 작성

--- backend/src/test/java/project/personalproject/domain/post/post/controller/PostControllerTest.java
+++ backend/src/test/java/project/personalproject/domain/post/post/controller/PostControllerTest.java
@@ -1,6 +1,7 @@
 package project.personalproject.domain.post.post.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,17 @@ import project.personalproject.domain.post.post.dto.request.CreatePostCommand;
 import project.personalproject.domain.post.post.service.PostService;
 import project.personalproject.global.security.jwt.JwtService;
 
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WithMockUser
 @WebMvcTest(PostController.class)
+@Slf4j
 class PostControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -81,5 +86,44 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.errors[0].field").value("content"));
 
         verifyNoInteractions(postService);
+    }
+
+    @Test
+    @DisplayName("게시글 생성 API")
+    void create_post() throws Exception {
+        // given : 제목 내용 이미지
+        var postJson = new MockMultipartFile(
+                "postRequest", "", "application/json",
+                objectMapper.writeValueAsBytes(new CreatePostCommand("제목", "내용"))
+        );
+        var image1 = new MockMultipartFile(
+                "images", "a.jpg", "image/jpeg", "dummy".getBytes()
+        );
+
+        when(postService.createPost(any(CreatePostCommand.class), any(), anyList()))
+                .thenReturn(null);
+
+        // when & then
+
+        // warm up
+        mockMvc.perform(multipart("/api/v1/post")
+                .file(postJson)
+                .file(image1)
+                .with(csrf()))
+                .andExpect(status().isOk());
+
+        // main
+        long startTime = System.nanoTime();
+        mockMvc.perform(multipart("/api/v1/post")
+                        .file(postJson)
+                        .file(image1)
+                        .with(csrf()))
+                .andExpect(status().isOk());
+        long endTime = System.nanoTime();
+
+        verify(postService, times(2)).createPost(any(CreatePostCommand.class), any(), anyList());
+
+        log.info("[게시글 생성 API] 소요 시간 : {} ms", endTime - startTime);
+
     }
 }
