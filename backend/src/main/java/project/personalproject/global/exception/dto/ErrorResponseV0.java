08214dd2b54a9408a17305b1f25871807f583b4d
HyeonSeong <gus8131@naver.com>
2025-08-14 14:38:22 +0900

    [#31] Refactor : 필드 단위 에러 반환 로직 추가

--- backend/src/main/java/project/personalproject/global/exception/dto/ErrorResponseV0.java
+++ backend/src/main/java/project/personalproject/global/exception/dto/ErrorResponseV0.java
@@ -1,16 +1,20 @@
 package project.personalproject.global.exception.dto;
 
 import lombok.Builder;
+import org.springframework.validation.BindingResult;
 import project.personalproject.global.exception.ErrorCode;
+
+import java.util.List;
 
 @Builder
 public record ErrorResponseV0(
         String name,
         int errorCode,
-        String message
+        String message,
+        List<FieldError> errors
 ) {
 
-    // 엔티티 -> DTO
+    // 도메인 & 서비스 에러
     public static ErrorResponseV0 of(ErrorCode errorCode) {
         return ErrorResponseV0.builder()
                 .name(errorCode.name())
@@ -18,4 +22,18 @@ public record ErrorResponseV0(
                 .message(errorCode.getMessage())
                 .build();
     }
+
+    // @Valid 바디 검증 실패
+    public static ErrorResponseV0 ofValidation(BindingResult br) {
+        List<FieldError> list = br.getFieldErrors().stream()
+                .map(fe -> new FieldError(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+        return ErrorResponseV0.builder()
+                .name("BAD_REQUEST")
+                .errorCode(400)
+                .message("Validation failed")
+                .errors(list)
+                .build();
+    }
+
 }
